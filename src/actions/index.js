2077a835ea041ca14c061e7f9193a092b7fb7b17
Richy Chen <richychn@Richys-MacBook-Pro.local>
2020-02-02 17:49:33 -0800

    finished blog app

---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPost = () => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts')
+        dispatch({type: 'FETCH_POST', payload: response.data})
+    }
+}
+
+export const fetchUser = (id) => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get(`/users/${id}`)
+        dispatch({type: 'FETCH_USER', payload: response.data})
+    }
+}
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPost());
+    
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+}
